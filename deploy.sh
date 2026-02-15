154740f793b14f053bead7229adcdb6f4ca30e1c
Meer Ali <syed.idea@outlook.com>
2026-02-15 17:50:21 +1000

    Updated deployment file

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 
 RESOURCE_GROUP=${1:?"Resource group is required"}
-PARAM_FILE=${2:-"variables/parameters.dev.json"}
+PARAM_FILE=${2:-"variable/parameter.dev.json"}
 
 az deployment group create \
   --resource-group "$RESOURCE_GROUP" \
-  --template-file bicep/main.bicep \
+  --template-file main.bicep \
   --parameters @"$PARAM_FILE"
